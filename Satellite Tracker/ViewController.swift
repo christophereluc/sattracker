7b77f13647ab5d2845baf4726f16c0c05ec26fa4
Christopher Luc <christopher.e.luc@gmail.com>
2019-10-06 15:17:14 -0700

    Initial Commit

---
+++ Satellite Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Satellite Tracker
+//
+//  Created by Christopher Luc on 10/6/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
