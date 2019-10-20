e168d01cd84bf1a60e76c0e75f54b34961ed9069
Christopher Luc <christopher.e.luc@gmail.com>
2019-10-20 15:40:37 -0700

    Added in location services in preparation of adding mock satellite

--- Satellite Tracker/ViewController.swift
+++ Satellite Tracker/ViewController.swift
@@ -8,35 +8,52 @@
 import UIKit
 import SceneKit
 import ARKit
+import CoreLocation
 
 @available(iOS 11.0, *)
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
-
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+
+        checkLocationServices()
         setupBasicARScene()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create the arworld config
         let configuration = ARWorldTrackingConfiguration()
-
         // And now run it
         sceneView.session.run(configuration)
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
+    override func viewDidDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
         // Pause the session when the view disappears
         sceneView.session.pause()
     }
     
+    func showLocationRequiredAlert() {
+        let settingsAppURL = URL(string: UIApplication.openSettingsURLString)!
+        let alert = UIAlertController(title: "Location Services Required", message: "Location services are required to track satellites relative to phone locations", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Allow Access", style: .default, handler:  {
+            (alert) -> Void in UIApplication.shared.open(settingsAppURL, options: [:], completionHandler: nil)
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+        self.present(alert, animated: true)
+    }
     
     func setupBasicARScene() {
         
@@ -52,7 +69,36 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.scene = SCNScene()
         
     }
-
-
+    
+    
+    
+    
+    func checkLocationServices() {
+    
+        let locationAuthorizationStatus = CLLocationManager.authorizationStatus()
+    
+        switch locationAuthorizationStatus {
+        case .notDetermined:
+            self.locationManager.requestWhenInUseAuthorization() // This is where you request permission to use location services
+        case .authorizedWhenInUse, .authorizedAlways:
+            if CLLocationManager.locationServicesEnabled() {
+                self.locationManager.startUpdatingLocation()
+            }
+        case .restricted, .denied:
+            showLocationRequiredAlert()
+        }
+    }
+        
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status != .authorizedAlways || status != .authorizedWhenInUse {
+            checkLocationServices()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
+    
 }
 
