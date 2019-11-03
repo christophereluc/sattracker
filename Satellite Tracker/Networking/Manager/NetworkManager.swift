cba585e63491b8ace7a6a7f00a8eaf667b522106
Christopher Luc <christopher.e.luc@gmail.com>
2019-11-02 22:22:56 -0700

    Added in classes to talk with API

---
+++ Satellite Tracker/Networking/Manager/NetworkManager.swift
@@ -0,0 +1,64 @@
+//
+//  NetworkManager.swift
+//  Satellite Tracker
+//
+//  Created by Christopher Luc on 11/2/19.
+//
+
+import Foundation
+import CoreLocation
+
+enum Result {
+    case success
+    case failure(String)
+}
+
+struct NetworkManager {
+    //MARK Instatiate router -- Can be multiple if we end up needing new api endpoitns
+    let router = Router<SatelliteApi>()
+    
+    //MARK
+    func getNearbySatellites(location: CLLocation, completion: @escaping (_ data: [Any]?,_ error: String?)->()){
+        router.request(.nearby(location: location)) { data, response, error in
+            //Error isn't nil; therefore network errors
+            if error != nil {
+                completion(nil, "Please check your network connection.")
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                //Check if we got a successful HTTP response
+                let result = self.handleNetworkResponse(response)
+                
+                switch result {
+                case .success:
+                    guard let responseData = data else {
+                        completion(nil, "Response data was null -- validate API correct")
+                        return
+                    }
+                    do {
+                        //MARK: This is where the json object should be converted into a data model.  Still a TODO
+                        let jsonData = try JSONSerialization.jsonObject(with: responseData, options: .mutableContainers)
+                        print(jsonData)
+                        
+                        //MARK pass in data object into completion handler
+                        completion(nil ,nil)
+                    } catch {
+                        completion(nil, "Error decoding data - \(error)")
+                    }
+                case .failure(let networkFailureError):
+                    completion(nil, networkFailureError)
+                }
+            }
+        }
+    }
+    
+    //Checks status code.  Only concerned with 200-299 at this point
+    fileprivate func handleNetworkResponse(_ response: HTTPURLResponse) -> Result {
+        switch response.statusCode {
+        case 200...299:
+            return .success
+        default:
+            return .failure("Network Request Failed -- status code outside valid range: \(response.statusCode)")
+        }
+    }
+}
---
+++ Satellite Tracker/Networking/Service/NetworkRouter.swift
@@ -0,0 +1,84 @@
+//
+//  NetworkRouter.swift
+//  Satellite Tracker
+//
+//  Created by Christopher Luc on 11/2/19.
+//  Based off of https://github.com/Mackis/NetworkLayer/blob/master/NetworkLayer/Networking/Service/NetworkROUTER.swift
+
+import Foundation
+
+//Typealiases to make methods more readable
+public typealias Parameters = [String:Any]
+public typealias NetworkRouterCompletion = (_ data: Data?,_ response: URLResponse?,_ error: Error?)->()
+
+
+//MARK: Router class that would handle setting up all API requests.  NetworkManager instanciates it
+class Router<EndPoint: EndPointType> {
+    private var task: URLSessionTask?
+    
+    func request(_ route: EndPoint, completion: @escaping NetworkRouterCompletion) {
+        let session = URLSession.shared
+        do {
+            let request = try self.buildRequest(from: route)
+            task = session.dataTask(with: request, completionHandler: { data, response, error in
+                completion(data, response, error)
+            })
+        }catch {
+            completion(nil, nil, error)
+        }
+        self.task?.resume()
+    }
+    
+    func cancel() {
+        self.task?.cancel()
+    }
+    
+    fileprivate func buildRequest(from route: EndPoint) throws -> URLRequest {
+        var request = URLRequest(url: route.baseURL.appendingPathComponent(route.path),
+                                 cachePolicy: .reloadIgnoringLocalAndRemoteCacheData,
+                                 timeoutInterval: 10.0)
+        
+        //MARK make this dynamic if we end up needing different methods
+        request.httpMethod = "GET"
+        
+        //MARK add other task types if added here
+        switch route.task {
+        case .request:
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        case .requestWithParameters(let urlParameters):
+            do {
+                try encode(urlRequest: &request, urlParameters: urlParameters)
+            } catch {
+                throw error
+            }
+        }
+        return request
+    }
+    
+    
+}
+
+//Mark extension to the router that adds in encoding capabilities
+extension Router {
+    
+    public func encode(urlRequest: inout URLRequest, urlParameters: Parameters?) throws {
+        //if no parameters passed in, bail early
+        guard let urlParameters = urlParameters else { return }
+        
+        if var urlComponents = URLComponents(url: urlRequest.url!, resolvingAgainstBaseURL: false), !urlParameters.isEmpty {
+            urlComponents.queryItems = [URLQueryItem]()
+            
+            //Append all query params to the URL
+            for (key,value) in urlParameters {
+                let queryItem = URLQueryItem(name: key, value: "\(value)".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed))
+                urlComponents.queryItems?.append(queryItem)
+            }
+            
+            urlRequest.url = urlComponents.url
+        }
+        if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+            urlRequest.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        }
+        
+    }
+}
