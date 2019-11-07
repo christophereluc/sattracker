d5430e59dc709fc5d1336752bfa31dfeba340ab0
Kamille Delgardo <hecate@hecate.local>
2019-11-06 23:18:49 -0800

    model work

--- Satellite Tracker/Position.swift
+++ Satellite Tracker/Position.swift
@@ -1,44 +1,20 @@
 //
 //  Position.swift
 //  Satellite Tracker
-//  Creates model for data returned by /tracking endpoint
 //
 //  Created by Kamille Delgardo on 11/6/19.
 //
-//  example returned data:
-/*      "startAz": 159.94,
-        "startAzCompass": "SSE",
-        "startUTC": 1573145495,
-        "maxAz": 86.7,
-        "maxAzCompass": "E",
-        "maxEl": 41.91,
-        "maxUTC": 1573146185,
-        "endAz": 14.9,
-        "endAzCompass": "N",
-        "endUTC": 1573146865
- */
-
-
 
 import Foundation
 
-enum Direction {
-    case N, NNE, NE, ENE, E, ESE, SE, SSE, S, SSW, SW, WSW, W, WNW, NW, NNW
-}
-
 class Position {
-    var startAz: Float?
-    var startAzCompass: Direction?
-    var startUTC: NSDate?
-    var maxAz: Float?
-    var maxAzCompass: Direction?
-    var maxEl: Float?
-    var maxUTC: NSDate?
-    var endAz: Float?
-    var endAzCompass: Direction?
-    var endUTC: NSDate?
+    var latitude: Float!
+    var longitude: Float!
+    var altitude: Float!
     
-    init()
-    
-
+    init(latitude: Float, longitude: Float, altitude: Float) {
+        self.latitude = latitude
+        self.longitude = longitude
+        self.altitude = altitude
+    }
 }
