382dfb5d3cde11d325c73abbbdc787b0bc18f64f
Kamille Delgardo <hecate@hecate.local>
2019-11-07 04:19:25 -0800

    models, sanitizing data

--- Satellite Tracker/Beacon.swift
+++ Satellite Tracker/Beacon.swift
@@ -3,7 +3,17 @@
 //  Satellite Tracker
 //
 //  Created by Kamille Delgardo on 11/6/19.
-//
+/*  Example API return data:
+    {
+        "name": "CubeBel-1(BSUSat-1)",
+        "id": "43666",
+        "uplink": "436.200",
+        "downlink": "436.200",
+        "beacon": "436.990*",
+        "mode": "4k8/9k6* GMSK Digipeater",
+        "callsign": "EU10S"
+    }  */
+
 
 import Foundation
 
