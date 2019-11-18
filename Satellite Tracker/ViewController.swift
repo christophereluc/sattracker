1a937979a46266541c0801986475e273827e730b
Christopher Luc <christopher.e.luc@gmail.com>
2019-11-17 16:39:35 -0800

    Moved code around into different extensions based on functionality.  Added in new image for ISS.  Added in timer that refreshes satellites every 2 minutes.

--- Satellite Tracker/ViewController.swift
+++ Satellite Tracker/ViewController.swift
@@ -12,117 +12,45 @@ import ARKit
 import CoreLocation
 
 @available(iOS 11.0, *)
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController {
     
     var locationData: [CLLocation] = []
     
     let sceneLocationView = SceneLocationView()
     let locationManager = CLLocationManager()
-    var coordinate: CLLocationCoordinate2D?
-    
     let networkManager = NetworkManager()
-    
+    var timer: Timer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
         
-        checkLocationServices()
         setupBasicARScene()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        checkLocationServices()
         sceneLocationView.run()
+
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         // Pause the session when the view disappears
         sceneLocationView.pause()
+        //Stop timer from firing if the screen is backgrounded
+        timer?.invalidate()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         sceneLocationView.frame = view.bounds
     }
-    
-    func showLocationRequiredAlert() {
-        let settingsAppURL = URL(string: UIApplication.openSettingsURLString)!
-        let alert = UIAlertController(title: "Location Services Required", message: "Location services are required to track satellites relative to phone locations", preferredStyle: .alert)
         
-        alert.addAction(UIAlertAction(title: "Allow Access", style: .default, handler:  {
-            (alert) -> Void in UIApplication.shared.open(settingsAppURL, options: [:], completionHandler: nil)
-        }))
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        
-        self.present(alert, animated: true)
-    }
-    
-    func setupBasicARScene() {
-        
-        sceneLocationView.locationNodeTouchDelegate = self
-        sceneLocationView.arViewDelegate = self
-        sceneLocationView.locationNodeTouchDelegate = self
-        sceneLocationView.orientToTrueNorth = true
-        
-        view.addSubview(sceneLocationView)
-        sceneLocationView.frame = view.bounds
-    }
-    
-    func checkLocationServices() {
-        
-        let locationAuthorizationStatus = CLLocationManager.authorizationStatus()
-        
-        switch locationAuthorizationStatus {
-        case .notDetermined:
-            self.locationManager.requestWhenInUseAuthorization() // This is where you request permission to use location services
-        case .authorizedWhenInUse, .authorizedAlways:
-            if CLLocationManager.locationServicesEnabled() {
-                self.locationManager.startUpdatingLocation()
-            }
-        case .restricted, .denied:
-            showLocationRequiredAlert()
-        }
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        if status != .authorizedAlways || status != .authorizedWhenInUse {
-            checkLocationServices()
-        }
-    }
-    
     var calledOnce = false
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
-        //print("locations = \(locValue.latitude) \(locValue.longitude)")
-        
-        
-        //MARK just a test to get the API call to occur
-        if calledOnce == false {
-            calledOnce = true
-            networkManager.getNearbySatellites(location: manager.location!, completion: testCompletionOfNearby)
-            networkManager.getBeacons(id: 41465, completion: testBeaconCompletion)
-            networkManager.getPath(id: 41465, location: manager.location!, completion: testPathCompletion)
-        }
-    }
-    
-    func testCompletionOfNearby(data: NearbySatellites?, error: String?) {
-        if let data = data {
-            //Rejoin main thread since this is called as a result of a bg threaded network call
-            DispatchQueue.main.async {
-                self.addSceneModels(satellites: data.satellites)
-                if let iss = data.iss {
-                    self.addSceneModels(satellites: [iss])
-                }
-            }
-        }
-        else if let error = error {
-            print(error)
-        }
-    }
     
     func testBeaconCompletion(data: BeaconResponse?, error: String?) {
         if let data = data {
@@ -141,11 +69,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             print(error)
         }
     }
-    
-    //MARK Empty function to pass in as completion block to network manager
-    func testCompletion(data: [Any]?, error: String?) {
-    }
-    
+  
 }
 
 // MARK: - LNTouchDelegate
@@ -166,8 +90,72 @@ extension ViewController: LNTouchDelegate {
     
 }
 
+extension DispatchQueue {
+    func asyncAfter(timeInterval: TimeInterval, execute: @escaping () -> Void) {
+        self.asyncAfter(
+            deadline: DispatchTime.now() + Double(Int64(timeInterval * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC),
+            execute: execute)
+    }
+}
+
+//Extension for ARKit/ARCL related methods
 @available(iOS 11.0, *)
 extension ViewController: ARSCNViewDelegate {
+    
+    func setupBasicARScene() {
+        
+        sceneLocationView.locationNodeTouchDelegate = self
+        sceneLocationView.arViewDelegate = self
+        sceneLocationView.locationNodeTouchDelegate = self
+        sceneLocationView.orientToTrueNorth = true
+        
+        view.addSubview(sceneLocationView)
+        sceneLocationView.frame = view.bounds
+    }
+    
+    /// Adds the appropriate ARKit models to the scene.  Note: that this won't
+    /// do anything until the scene has a `currentLocation`.  It "polls" on that
+    /// and when a location is finally discovered, the models are added.
+    func addSceneModels(satellites: [NearbySatellite], imageName: String) {
+        // 1. Don't try to add the models to the scene until we have a current location
+        guard sceneLocationView.sceneLocationManager.currentLocation != nil else {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
+                self?.addSceneModels(satellites: satellites, imageName: imageName)
+            }
+            return
+        }
+        sceneLocationView.removeAllNodes()
+        satellites.forEach { satellite in
+            //N2YO returns altiltue as KM, but CoreLocation expects altitude in meters, so convert before passing in.
+            let node = buildNode(latitude: satellite.satlat, longitude: satellite.satlng, altitude: (satellite.satalt * 1000), imageName: imageName)
+            sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: node)
+        }
+    }
+    
+    func buildNode(latitude: CLLocationDegrees, longitude: CLLocationDegrees,
+                   altitude: CLLocationDistance, imageName: String) -> LocationAnnotationNode {
+        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let location = CLLocation(coordinate: coordinate, altitude: altitude)
+        let image = UIImage(named: imageName)!
+        let imageView = UIImageView(image: image)
+
+        let node = LocationAnnotationNode(location: location, view: imageView)
+        return setNodeViewAndTag(node: node, location: location, view: imageView)
+
+    }
+    
+    
+
+    //MARK This is hacky, but is resolved in ARCL 1.2.2, so remove this if library updated before DEC
+    func setNodeViewAndTag(node: LocationAnnotationNode, location: CLLocation, view: UIView) -> LocationAnnotationNode {
+        //Tag will be the index of the inserted CLLocation
+        let tag = locationData.count
+        locationData.append(location)
+        //Now set tag for retrieval later
+        view.tag = tag
+        node.annotationNode.view = view
+        return node
+    }
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         print("Added SCNNode: \(node)")    // you probably won't see this fire
@@ -180,60 +168,97 @@ extension ViewController: ARSCNViewDelegate {
     func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
         print("Camera: \(camera)")
     }
+}
+
+//MARK Location services extension
+extension ViewController: CLLocationManagerDelegate {
+    func checkLocationServices() {
+        
+        let locationAuthorizationStatus = CLLocationManager.authorizationStatus()
+        
+        switch locationAuthorizationStatus {
+        case .notDetermined:
+            self.locationManager.requestWhenInUseAuthorization() // This is where you request permission to use location services
+        case .authorizedWhenInUse, .authorizedAlways:
+            if CLLocationManager.locationServicesEnabled() {
+                handleAuthorized()
+            }
+            //MARK Handle services not enabled?
+        case .restricted, .denied:
+            showLocationRequiredAlert()
+        }
+    }
+    
+    //Handles the authorized case
+    func handleAuthorized() {
+        self.locationManager.startUpdatingLocation()
+        //If old timer isn't null, invalidate it
+        timer?.invalidate()
+        //Start a new timer
+        timer = Timer.scheduledTimer(withTimeInterval: 120, repeats: true, block: updateSatellites(timer:))
+        //Immediately fire the timer so that we can get our first set of data
+        timer?.fire()
+    }
+    
+    //Shows the dialog for displaying why we need location services permission
+    func showLocationRequiredAlert() {
+        let settingsAppURL = URL(string: UIApplication.openSettingsURLString)!
+        let alert = UIAlertController(title: "Location Services Required", message: "Location services are required to track satellites relative to phone locations", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Allow Access", style: .default, handler:  {
+            (alert) -> Void in UIApplication.shared.open(settingsAppURL, options: [:], completionHandler: nil)
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true)
+    }
+    
+    //Delegate method called when authorization status is changed
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        timer?.invalidate()
+        checkLocationServices()
+    }
+    
+    //Called when location changes
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        
+        //MARK just a test to get the API call to occur
+        if calledOnce == false {
+            calledOnce = true
+            networkManager.getBeacons(id: 41465, completion: testBeaconCompletion)
+            networkManager.getPath(id: 41465, location: manager.location!, completion: testPathCompletion)
+        }
+    }
     
 }
 
-extension DispatchQueue {
-    func asyncAfter(timeInterval: TimeInterval, execute: @escaping () -> Void) {
-        self.asyncAfter(
-            deadline: DispatchTime.now() + Double(Int64(timeInterval * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC),
-            execute: execute)
-    }
-}
-
-
-@available(iOS 11.0, *)
+//Extension to handle network requests/responses
 extension ViewController {
     
-    /// Adds the appropriate ARKit models to the scene.  Note: that this won't
-    /// do anything until the scene has a `currentLocation`.  It "polls" on that
-    /// and when a location is finally discovered, the models are added.
-    func addSceneModels(satellites: [NearbySatellite]) {
-        // 1. Don't try to add the models to the scene until we have a current location
-        guard sceneLocationView.sceneLocationManager.currentLocation != nil else {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
-                self?.addSceneModels(satellites: satellites)
-            }
-            return
+    func updateSatellites(timer: Timer) {
+        if let location = locationManager.location {
+            networkManager.getNearbySatellites(location: location, completion: handleNearbySatelliteResults(data:error:))
         }
-                    
-        satellites.forEach { satellite in
-            //N2YO returns altiltue as KM, but CoreLocation expects altitude in meters, so convert before passing in.
-            let node = buildNode(latitude: satellite.satlat, longitude: satellite.satlng, altitude: (satellite.satalt * 1000))
-            sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: node)
+        else {
+            //MARK handle error case with dialog
+            print("error with location?")
         }
     }
     
-    func buildNode(latitude: CLLocationDegrees, longitude: CLLocationDegrees,
-                   altitude: CLLocationDistance) -> LocationAnnotationNode {
-        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        let location = CLLocation(coordinate: coordinate, altitude: altitude)
-        let image = UIImage(named: "satellite")!
-        let imageView = UIImageView(image: image)
-
-        let node = LocationAnnotationNode(location: location, view: imageView)
-        return setNodeViewAndTag(node: node, location: location, view: imageView)
-
-    }
-
-    //MARK This is hacky, but is resolved in ARCL 1.2.2, so remove this if library updated before DEC
-    func setNodeViewAndTag(node: LocationAnnotationNode, location: CLLocation, view: UIView) -> LocationAnnotationNode {
-        //Tag will be the index of the inserted CLLocation
-        let tag = locationData.count
-        locationData.append(location)
-        //Now set tag for retrieval later
-        view.tag = tag
-        node.annotationNode.view = view
-        return node
+    func handleNearbySatelliteResults(data: NearbySatellites?, error: String?) {
+        if let data = data {
+            //Rejoin main thread since this is called as a result of a bg threaded network call
+            DispatchQueue.main.async {
+                self.addSceneModels(satellites: data.satellites, imageName: "satellite")
+                if let iss = data.iss {
+                    self.addSceneModels(satellites: [iss], imageName: "iss")
+                }
+            }
+        }
+        else if let error = error {
+            //MARK handle error case with dialog
+            print(error)
+        }
     }
 }
